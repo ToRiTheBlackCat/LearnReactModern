e7e81866bc2e519553ad26858bf9c5a8837a1df5
TriNHM <triminh0502@gmail.com>
2026-03-20 10:29:23 +0700

    pure/impure components

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Lesson6 from "./lessons/Lesson6/Lesson6";
 import Lesson7 from "./lessons/Lesson7/Lesson7";
 import Lesson8 from "./lessons/Lesson8/Lesson8";
 import Lesson9 from "./lessons/Lesson9/Lesson9";
-
+import Lesson10 from "./lessons/Lesson10/Lesson10";
 const App = () => {
   const [count, setCount] = useState(0);
 
@@ -21,7 +21,7 @@ const App = () => {
         count={count}
         setCount={setCount}
       /> */}
-      <Lesson9 />
+      <Lesson10 />
       <Footer />
     </div>
   );
